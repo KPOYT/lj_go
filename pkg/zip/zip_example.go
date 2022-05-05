a2f2487c5347e51d02cd73bee9f6d9137517ac91
Jack Bizzell <lambdajack@protonmail.com>
2022-05-05 20:02:10 +0100

    adding exampe zip.Unzip

--- pkg/zip/zip_example.go
+++ pkg/zip/zip_example.go
@@ -1,1 +1,16 @@
 package zip
+
+import (
+	"fmt"
+	"log"
+)
+
+func exUnzip() {
+
+	files, err := Unzip("path/to/zip.zip", "path/to/out/dir")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(files)
+}
