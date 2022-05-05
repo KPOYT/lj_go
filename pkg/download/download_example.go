bfe5a34f0768079ee5f323a312558cf2a0c21dcb
Jack Bizzell <lambdajack@protonmail.com>
2022-05-05 21:12:32 +0100

    FromURL exemple

--- pkg/download/download_example.go
+++ pkg/download/download_example.go
@@ -1,1 +1,21 @@
 package download
+
+import (
+	"log"
+)
+
+//lint:ignore U1000 unused example;
+func exFromURL() {
+
+	// Specify the destination file name ("we_love_robots.txt")
+	err := FromURL("https://duckduckgo.com/robots.txt", "path/to/out/dir", "we_love_robots.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Calculate the destination file name from the URL ("robots.txt")
+	err = FromURL("https://duckduckgo.com/robots.txt", "path/to/out/dir", "")
+	if err != nil {
+		log.Fatal(err)
+	}
+}
