ef465fe50127c797642ca1309c02a5233b1829df
Jack Bizzell <lambdajack@protonmail.com>
2022-05-06 02:35:32 +0100

    adding json.DecodeTo example

--- pkg/json/json_example.go
+++ pkg/json/json_example.go
@@ -1,1 +1,20 @@
 package json
+
+import "log"
+
+type jsonStruct struct {
+	Test string `json:"test"`
+	Names []string `json:"names"`
+}
+
+//lint:ignore U1000 unused example;
+func exDecodeTo() {
+
+	j := jsonStruct{}
+
+	err := DecodeTo(&j, "../../internal/test/data/json/test.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
